04aa880072376b9def00b104784984006228788a
zarshad <a.zarshad@rayatak.ir>
2019-09-01 17:24:47 +0430

    add sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import TestState from './copmonents/test/TestState';
+import Gradient from './copmonents/gradient';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <TestState/>
+      {/* <TestState/> */}
+      <Gradient/>
     </div>
   );
 }
